190c2135165def2d2120dc1fbf7de295dfaa16e2
Giovannimbesi25 <imbesigiovanni250400@gmail.com>
2024-03-20 19:45:37 +0100

    ingress

--- productFrontend/src/services/ProductService.js
+++ productFrontend/src/services/ProductService.js
@@ -4,7 +4,7 @@ const API_BASE_URL = process.env.REACT_APP_URL || 'http://localhost:8080/api/pro
 
 // const API_BASE_URL = "http://product-backend-service:8080/api/products";
 
-console.log("Prova", process.env.API_BASE_URL)
+console.log("Prova", process.env.REACT_APP_URL)
 
 const ProductService = {
   getAllProducts: (limit) => axios.get(`${API_BASE_URL}?limit=${limit}`),
