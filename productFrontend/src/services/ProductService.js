5b9eb516cba0be7079fa286a1a48f9e6950135a2
Giovannimbesi25 <imbesigiovanni250400@gmail.com>
2024-03-21 10:20:33 +0100

    change service url again

--- productFrontend/src/services/ProductService.js
+++ productFrontend/src/services/ProductService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/products';
+const API_BASE_URL = process.env.REACT_APP_URL || 'http://localhost:8080/api/products';
 
 // const API_BASE_URL = "http://product-backend-service:8080/api/products";
 
