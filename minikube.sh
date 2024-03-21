7d53fff8c52e3fce79ff3477c38541b2eaae96ed
Giovannimbesi25 <imbesigiovanni250400@gmail.com>
2024-03-21 09:56:29 +0100

    prova service url

--- minikube.sh
+++ minikube.sh
@@ -1,4 +1,17 @@
+minikube stop
+minikube delete
 minikube start
 kubectl apply -f deployments/mysql.yaml
 kubectl apply -f deployments/product-frontend.yaml
 kubectl apply -f deployments/product-backend.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/cloud/deploy.yaml
+
+
+kubectl get pods --namespace ingress-nginx
+
+
+minikube addons enable ingress
+kubectl apply -f deployments/ingress.yaml
+minikube ip
