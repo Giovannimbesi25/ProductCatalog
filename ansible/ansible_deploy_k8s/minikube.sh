627da9f2d0e70aa39acb11db449cf4c694f3affc
Giovannimbesi25 <imbesigiovanni250400@gmail.com>
2024-03-22 17:02:11 +0100

    try to reduce frontend images

--- ansible/ansible_deploy_k8s/minikube.sh
+++ ansible/ansible_deploy_k8s/minikube.sh
@@ -1,6 +1,3 @@
-# minikube stop
-# minikube delete
-# minikube start
 kubectl apply -f ../../deployments/mysql.yaml
 kubectl apply -f ../../deployments/product-frontend.yaml
 kubectl apply -f ../../deployments/product-backend.yaml
