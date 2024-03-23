57fc8b62fd0d5113016b4fd794aca1ce67c62e14
Giovannimbesi25 <imbesigiovanni250400@gmail.com>
2024-03-23 11:19:26 +0100

    prova ansible

--- ansible/ansible_deploy_k8s/minikube.sh
+++ ansible/ansible_deploy_k8s/minikube.sh
@@ -2,6 +2,7 @@ kubectl apply -f deployments/product-frontend.yaml
 kubectl apply -f deployments/product-backend.yaml
 minikube addons enable ingress
 kubectl apply -f deployments/ingress.yaml
+kubectl apply -f deployments/ingress.yaml
 minikube ip
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
